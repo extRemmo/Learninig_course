c8cfb78d275b8775537c615ac1305abc72753ed2
Student52 <dmitry.gra4ev@gmail.com>
2020-04-07 13:25:21 +0300

    Input line has been deleted

--- src/main.java
+++ src/main.java
@@ -1,7 +1,5 @@
 public class main {
     public static void main (String[] args) {
-        String Name = "Dima";
-        System.out.printf("Name: %s", Name);
-        //System.out.print("Hello,world! \n");
+        System.out.print("Hello,world! \n");
     }
 }
