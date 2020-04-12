de818c64e28cdf50f8925fb5136a90e0ff411b6a
Student52 <dmitry.gra4ev@gmail.com>
2020-04-12 15:18:19 +0300

    Binary input - dec output

--- src/console.java
+++ src/console.java
@@ -1,13 +1,17 @@
+/*
+    Ввести с консоли число в бинарном формате. Перевести его в десятичный формат, записать в переменную int и вывести на экран.
+ */
+
 import java.util.Scanner;
+
 public class console {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число в бинарном формате: ");
+        String bin = in.nextLine();
+        int Num = Integer.parseInt(bin, 2);
+        System.out.println("Введено число:" + Num);
 
-            Scanner in = new Scanner(System.in);
-            System.out.print("Input a number: ");
-            int num = in.nextInt();
-
-            System.out.printf("Your number: %d \n", num);
-        }
     }
-
+}
