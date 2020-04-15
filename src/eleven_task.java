c6ff49260b30b793ae7ec06a59bed3e38b41a5b8
Student52 <dmitry.gra4ev@gmail.com>
2020-04-15 16:58:40 +0300

    Task 11. my first string

---
+++ src/eleven_task.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class eleven_task {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        System.out.print("Вы ввели строку: "+ str);
+    }
+}
