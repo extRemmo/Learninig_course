add87aa81a9aca7288c3c326938b8a6cbf642c90
Student52 <dmitry.gra4ev@gmail.com>
2020-04-19 20:17:07 +0300

    Task 18. rewriter

--- src/eighteen_task.java
+++ src/eighteen_task.java
@@ -5,10 +5,12 @@ public class eighteen_task {
     public static void main(String[] args) {
         eighteen_task.read(args); //вызвали метод чтения из файла
 
+
         String str1;
         BufferedReader br =
                 new BufferedReader(
                         new InputStreamReader(System.in));
+        System.out.println("Введите новые данные в файл");
         System.out.println("Признак конца ввода - строка 'stop'");
 
         try (FileWriter fw = new FileWriter("C:/Users/user/Desktop/test.txt")) { //перезапись в тот же файл
@@ -28,13 +30,17 @@ public class eighteen_task {
 
     public static void read (String[] args) {
         String S;
+        int countline = 0;
         try (BufferedReader br = new BufferedReader(new FileReader("C:/Users/user/Desktop/test.txt")))
         {
             while ((S = br.readLine()) != null){
-                System.out.println("Текущее содержимое файла: " + S);
+                System.out.println(S);
+                countline++;
             }
+            System.out.println("Количество строк в текущем файле: " + countline);
         } catch (IOException exc) {
             System.out.println("Ошибка ввода-вывода" + exc);
         }
+
 }
 }
