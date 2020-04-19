ac2d09f246cd79f96a84f47732ffc8b2aa8438e5
Student52 <dmitry.gra4ev@gmail.com>
2020-04-19 12:13:47 +0300

    Task 17. file writer

---
+++ src/seventeen_task.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class seventeen_task {
+    public static void main(String[] args) {
+        String str;
+        BufferedReader br =
+                new BufferedReader(
+                        new InputStreamReader(System.in));
+        System.out.println("Признак конца ввода - строка 'stop'");
+
+        try (FileWriter fw = new FileWriter("C:/Users/user/Desktop/test.txt")) {
+            do {
+                System.out.println(": ");
+                str = br.readLine();
+
+                if (str.compareTo("stop") == 0) break;
+
+                str = str + "\r\n";
+                fw.write(str);
+            } while (str.compareTo("stop") != 0);
+        } catch (IOException exc) {
+            System.out.println("Ошибка ввода-вывода: " + exc);
+        }
+    }
+}
