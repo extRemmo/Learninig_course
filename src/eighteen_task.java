d5e9e297398d7540e34c562ddc3af3d34cf3e195
Student52 <dmitry.gra4ev@gmail.com>
2020-05-18 10:53:43 +0300

    18 переделан ввод по кол-ву строк

--- src/eighteen_task.java
+++ src/eighteen_task.java
@@ -1,48 +1,54 @@
 
 
-        import java.io.*;
+import java.io.*;
 
 
-        public class eighteen_task {
+public class eighteen_task {
+        private static int countline = 0;
         public static void main(String[] args) {
-        eighteen_task.read(args); //вызвали метод чтения из файла
+                eighteen_task.read(args); //вызвали метод чтения из файла
+                //int countline = eighteen_task.read(args);
 
 
-        String str1;
-        BufferedReader br =
-        new BufferedReader(
-        new InputStreamReader(System.in));
-        System.out.println("Введите новые данные в файл");
-        System.out.println("Признак конца ввода - строка 'stop'");
+                String str1;
+                BufferedReader br =
+                        new BufferedReader(
+                                new InputStreamReader(System.in));
+                System.out.println("Введите новые данные в файл");
+                System.out.println("Признак конца ввода - строка 'stop'");
 
-        try (FileWriter fw = new FileWriter("C:/Users/user/Desktop/test.txt")) { //перезапись в тот же файл
-        do {
-        System.out.println(": ");
-        str1 = br.readLine();
+                try (FileWriter fw = new FileWriter("C:/Users/user/Desktop/test.txt")) { //перезапись в тот же файл
+                        int countline2 = 0;
+                        do {
+                                if (countline2 == eighteen_task.countline) break;
+                                System.out.println(": ");
+                                str1 = br.readLine();
 
-        if (str1.compareTo("stop") == 0) break;
+                                if (str1.compareTo("stop") == 0) break;
 
-        str1 = str1 + "\r\n";
-        fw.write(str1);
-        } while (str1.compareTo("stop") != 0);
-        } catch (IOException exc) {
-        System.out.println("Ошибка ввода-вывода: " + exc);
-        }
+                                str1 = str1 + "\r\n";
+                                fw.write(str1);
+                                countline2++;
+                        } while (str1.compareTo("stop") != 0);
+                        System.out.println("Количество строк в перезаписанном файле: " + countline2);
+                } catch (IOException exc) {
+                        System.out.println("Ошибка ввода-вывода: " + exc);
+                }
         }
 
         public static void read (String[] args) {
-        String S;
-        int countline = 0;
-        try (BufferedReader br = new BufferedReader(new FileReader("C:/Users/user/Desktop/test.txt")))
-        {
-        while ((S = br.readLine()) != null){
-        System.out.println(S);
-        countline++;
-        }
-        System.out.println("Количество строк в текущем файле: " + countline);
-        } catch (IOException exc) {
-        System.out.println("Ошибка ввода-вывода" + exc);
-        }
+                String S;
+                //int countline = 0;
+                try (BufferedReader br = new BufferedReader(new FileReader("C:/Users/user/Desktop/test.txt")))
+                {
+                        while ((S = br.readLine()) != null){
+                                System.out.println(S);
+                                countline++;
+                        }
+                        System.out.println("Количество строк в текущем файле: " + countline);
+                } catch (IOException exc) {
+                        System.out.println("Ошибка ввода-вывода" + exc);
+                }
 
         }
-        }
+}
